49db0911dd301937e4cafe84b5ab7a2c7acc1909
Anzar Shaikh <anzarshaikh373@gmail.com>
2021-06-29 15:47:29 +0530

    Employee present or absent check done.

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,25 @@
 package com.bl.corejava;
+import java.util.Random;
 
+class utilityEmpWage{
+    Random rand = new Random();
+    public int isPresent = 1;
+    public int isAbsent = 2;
+
+    public void present() {
+        int check = rand.nextInt(2)+1;
+        if (check == isAbsent) {
+            System.out.println("Employee is absent.");
+        }
+        else {
+            System.out.println("Employee is present.");
+        }
+    }
+}
 public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation problem.");
+        utilityEmpWage empWage = new utilityEmpWage();
+        empWage.present();
     }
 }
